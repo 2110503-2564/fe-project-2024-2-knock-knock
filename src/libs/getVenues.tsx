1c6a957c7751d64412a4a40e5c1742999fbcf1d3
Sripipat Subannaphab <triplesxa@gmail.com>
2025-03-21 16:39:36 +0700

    BackEndModified

--- src/libs/getVenues.tsx
+++ src/libs/getVenues.tsx
@@ -4,7 +4,7 @@ export default async function getVenues() {
   await new Promise((resolve) => setTimeout(resolve, 300));
 
   const response = await fetch(
-    "http://localhost:5000/api/v1/hotels"
+    "http://newhotels-env.eba-qbmbbabk.us-east-1.elasticbeanstalk.com/api/v1/hotels"
   );
   if (!response.ok) {
     throw new Error("Failed to fetch cars");
